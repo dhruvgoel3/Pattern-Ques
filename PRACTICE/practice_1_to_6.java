62512ae5a6b70f6e4abb22ad835a1c62b2dfd05d
dhruvgoel3 <dhruvgoel1100@gmail.com>
2025-08-03 17:44:31 +0530

    pattern_ques

--- PRACTICE/practice_1_to_6.java
+++ PRACTICE/practice_1_to_6.java
@@ -14,7 +14,6 @@ public class practice_1_to_6 {
         }
 
     }
-
     static void pattern2(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
@@ -26,9 +25,9 @@ public class practice_1_to_6 {
     }
 
     static void pattern3(int n) {
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(1);
             }
             System.out.println();
         }
@@ -36,13 +35,42 @@ public class practice_1_to_6 {
     }
     static void pattern4(int n)
     {
-        
+        for(int i = 1; i<n; i++)
+        {
+            for(int j = 1; j<=i; j++) 
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern5(int n)
+    {
+        for(int i = 0; i<=n; i++)
+        {
+            for(int j = 0; j<n-i+1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern6(int n)
+    {
+        for(int i = 0; i<=n; i++)
+        {
+            for(int j = 1; j<n-i+1; j++)
+            {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern3(n);
+        pattern6(n);
 
     }
 
